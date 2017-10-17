ed6946107d75aa2cba823b856a5b908b70e811a4
Shreyas Lad <shadowtemplates@gmail.com>
2017-10-17 10:49:46 -0700

    Create bg.js

---
+++ js/bg.js
@@ -0,0 +1,3 @@
+var bgcolorlist=new Array("#228B22", "#FFD700", "#ADFF2F", "#FF69B4", "#CD5C5C", "#4B0082", "#7CFC00", "#ADD8E6", "#E84643", "#ED0A07", "#EA2907", "#E5294B", "#E00D26", "#FF3030", "#FC7500", "#F95700", "#F43900", "#F95620") 
+
+document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
